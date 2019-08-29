83bd9990a3e5f033ab5ceb1e8b60bbc50a756e73
Drassleon <toconasalvaro@gmail.com>
2019-08-28 19:43:47 -0500

    Added Post Transaction for Dispenser
    Signed-off-by: Drassleon <toconasalvaro@gmail.com>

--- app/src/main/java/Repository/FormRepository.kt
+++ app/src/main/java/Repository/FormRepository.kt
@@ -1,14 +1,16 @@
 package Repository
 
-import dto.Form
 import dto.ResponseDTO
-import okhttp3.Response
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface FormRepository{
         @GET("water-dispensers")
         fun getCharacterByName(@Query("id")id : String,@Query("water")water : String): Call<ResponseDTO>
+
+        @POST("{host}/dispension")
+        fun postDispenser(@Query("nombre") name: String,@Query("cantidadAgua") waterAmount: String): Call<ResponseBody>
 }
--- app/src/main/java/pe/edu/upc/pocdrinkwell/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/pocdrinkwell/MainActivity.kt
@@ -3,6 +3,7 @@ package pe.edu.upc.pocdrinkwell
 import Repository.FormRepository
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -20,9 +21,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var submitButton : Button
     lateinit var waterInput : EditText
     lateinit var hostInput : EditText
+    var hostGlob : String = ""
+    var userName : String = "UserName"
 
     private val formRepository = RetrofitConfig().getRetrofitInstance().create(FormRepository::class.java)
-    var id : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,37 +41,55 @@ class MainActivity : AppCompatActivity() {
             var host : String
             var water : String
 
-            host = hostInput.text.toString()
+            host = "AndroidAP"
             water = waterInput.text.toString()
             Log.d("Networking","Button Pressed")
-            formRepository.getCharacterByName(host,water).enqueue(object: Callback<ResponseDTO>{
-                override fun onFailure(call: Call<ResponseDTO>, t: Throwable) {
-                    Toast.makeText(this@MainActivity,"Could not connect to endpoint",Toast.LENGTH_SHORT).show()
-                    Log.d("NetworkingError","Could not connect to endpoint",t)
-                }
-
-                override fun onResponse(call: Call<ResponseDTO>, response: Response<ResponseDTO>) {
-                    if(response.body()==null)
-                    {
-                        Log.d("NetworkingSuccess", "Body is Null")
-                        Toast.makeText(this@MainActivity,"Connected to endpoint successfully",Toast.LENGTH_SHORT).show()
-                        return
-                    }
-                    else
-                    {
-                        Log.d("NetworkingSuccess", response.toString())
-                        Toast.makeText(this@MainActivity,"Connected to endpoint successfully",Toast.LENGTH_SHORT).show()
-
-                        var aux = response.body() as ResponseDTO
-
-                        var obj = aux.obj as Form
-                        var host = obj.host as String
-                        Log.d("NetworkingSuccess",host)
-
-                        return
-                    }
-                }
-            })
+            getHost(host,water)
+            postDispenserEvent()
         }
+    }
+    private fun getHost(host: String, water: String){
+        formRepository.getCharacterByName(host,water).enqueue(object: Callback<ResponseDTO>{
+            override fun onFailure(call: Call<ResponseDTO>, t: Throwable) {
+                Toast.makeText(this@MainActivity,"Could not connect to endpoint",Toast.LENGTH_SHORT).show()
+                Log.d("Networking","Could not connect to endpoint",t)
+            }
+
+            override fun onResponse(call: Call<ResponseDTO>, response: Response<ResponseDTO>) {
+                if(response.body()==null)
+                {
+                    Log.d("Networking", "Body is Null")
+                    Toast.makeText(this@MainActivity,"Connected to endpoint successfully",Toast.LENGTH_SHORT).show()
+                    return
+                }
+                else
+                {
+                    Log.d("Networking", response.toString())
+                    Toast.makeText(this@MainActivity,"Connected to endpoint successfully",Toast.LENGTH_SHORT).show()
+
+                    var aux = response.body() as ResponseDTO
+
+                    var obj = aux.obj as Form
+                    hostGlob = obj.host as String
+                    Log.d("Networking",host)
+                    return
+                }
+            }
+        })
+    }
+    private fun postDispenserEvent(){
+
+        formRepository.postDispenser(name = userName,waterAmount = waterInput.text.toString()).enqueue(object: Callback<ResponseBody>{
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                Log.d("Networking","Post Successful")
+                Toast.makeText(this@MainActivity,"Post Successful",Toast.LENGTH_SHORT).show()
+                finish()
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Toast.makeText(this@MainActivity,"Post failed!",Toast.LENGTH_SHORT).show()
+                Log.d("Networking","Post Failed!",t)
+            }
+        })
     }
 }
